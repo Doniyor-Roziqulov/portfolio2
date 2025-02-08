e56c116d52c6d1de8cf1b9bd0c3cbd0d0a26319d
Doniyor Ro'ziqulov <doniyorruziqulov33@gmail.com>
2025-02-09 00:58:07 +0500

    Added

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,7 +1,36 @@
 import { RxOpenInNewWindow } from "react-icons/rx";
 import { lists } from "../../static";
+import { lists1 } from "../../static";
 
 const Portfolio = () => {
+  const product1 = lists1.map((e, inx) => (
+    <div
+      key={inx}
+      className="overflow-hidden relative pro_bigdiv border h-[200px] min-[420px]:h-[250px] lg:h-[270px] border-neutral-500 shadow-2xl shadow-black"
+    >
+      <img
+        className="pro__img w-full h-full z-10 object-cover opacity-75"
+        src={e.img}
+        alt=""
+      />
+      <div
+        className="absolute z-20 top-[60px] lg:top-[100px] flex gap-y-3 left-[70px] xl:left-[150px] min-[420px]:top-[80px] min-[420px]:left-[110px]  min-[510px]:top-[80px] min-[510px]:left-[40px] flex-col items-center pro_div
+       min-[680px]:top-[80px] min-[680px]:left-[70px] min-[768px]:top-[90px] min-[768px]:left-[110px] lg:left-[70px]"
+      >
+        <p className="text-2xl pro__name font-bold text-sky-400">{e.name}</p>
+        <a
+          className="text-black text-lg items-center flex opacity-1 lg:opacity-0 gap-x-1 bg-sky-400 px-4 py-1 border hover:border-black hover:bg-transparent"
+          target="_blank"
+          key={inx}
+          href={e.link}
+        >
+          <RxOpenInNewWindow />
+          View
+        </a>
+      </div>
+    </div>
+  ));
+
   const product = lists.map((e, inx) => (
     <div
       key={inx}
@@ -35,6 +64,12 @@ const Portfolio = () => {
         <p className="text-[80px] lg:text-[150px] pointer-events-none text-skills z-20 text-slate-600 font-semibold absolute top-0 lg:left-[200px] xl:left-[400px]">
           Portfolio
         </p>
+        <p className="text-3xl font-semibold my-4 pl-3 pointer-events-none">
+          Real projects:
+        </p>
+        <div className="grid min-[510px]:grid-cols-2 lg:grid-cols-3 gap-2">
+          {product1}
+        </div>
         <p className="text-3xl font-semibold my-4 pl-3 pointer-events-none">
           Demo projects:
         </p>
--- src/static/index.ts
+++ src/static/index.ts
@@ -1,6 +1,21 @@
 import url1 from "../images/snapgram.png";
 import url2 from "../images/shopco.png";
 import url3 from "../images/nest.png";
+import url4 from "../images/img1.jpg";
+import url5 from "../images/img2.jpg";
+
+export const lists1 = [
+  {
+    name: "NSG Group",
+    link: "https://www.ncg-group.co.kr/",
+    img: url4,
+  },
+  {
+    name: "EnergyAudit",
+    link: "https://www.energyaudit.uz/",
+    img: url5,
+  },
+];
 
 export const lists = [
   {
